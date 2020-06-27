85743d78360122b71342130bee14d64dc750f0f3
Niloofar Zamani <niloofar.zamani92@gmail.com>
2020-06-27 18:32:49 +0430

    newly coded by materialize css

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import M from 'materialize-css';
+import '../css/style.scss';
+import '../files/fonts/Poppins-Regular.ttf';
+import '../files/atrium.jpg';
+import '../files/bedroom.jpg';
+import '../files/diningroom.jpg';
+import '../files/kitchenconcrete.jpg';
+import '../files/livingroom.jpg';
+import '../files/livingroom2.jpg';
+import '../files/team1.jpg';
+import '../files/team2.jpg';
+import '../files/team3.jpg';
+
+M.AutoInit();
